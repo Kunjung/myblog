470fee425e57a7dec95419ffd934f63a6f8e8334
Kunjung Sherpa <kunjungsherpa@gmail.com>
2016-11-17 15:49:44 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, url_for, redirect
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+		
+@app.route('/blog')
+def blog():
+	return render_template('blog.html')
+
+@app.route('/gallery')
+def gallery():
+	return render_template('gallery.html')
+
+@app.route('/signup')
+def signup():
+	if request.method == 'GET':
+		return render_template('signup.html')
+	
+
+@app.route('/mars', methods=['POST'])
+def mars():
+	name = request.form['name']
+	secret = request.form['password']
+
+	if name == '' or secret == '':
+		return redirect(url_for('signup'))
+	
+	return render_template('mars.html', name=name)
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+
+
+if __name__ == '__main__':
+	
+	# Debug Mode
+	#############
+	#app.run(debug=True)
+
+	# final mode
+	#############
+	#app.secret_key = os.urandom(12)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
